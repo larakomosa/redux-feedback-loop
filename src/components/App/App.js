abe0549d510794648db725d81d8ad6922a7edf42
Lara Komosa <larakomosa@gmail.com>
2020-11-06 17:39:03 -0600

    Brought in Dependencies, Direct Routes for Pages on App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react';
 //import axios from 'axios';
 import './App.css';
 
+//import redux and routers
+import { HashRouter as Router, Route } from 'react-router-dom';
+
+//import pages
+import Feeling from '../../pages/Feeling/Feeling';
+import Support from '../../pages/Support/Support';
+import Understanding from '../../pages/Understanding/Understanding';
+import Comments from '../../pages/Comments/Comments';
+import Review from '../../pages/Review/Review';
+import Submit from '../../pages/Submit/Submit';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Feedback!</h1>
-          <h4>
-            <i>Don't forget it!</i>
-          </h4>
         </header>
-        <br />
+        <Router>
+          <Route exact path="/" component={Feeling} />
+          <Route exact path="/understanding" component={Understanding} />
+          <Route path="/support" component={Support} />
+          <Route path="/comments" component={Comments} />
+          <Route path="/review" component={Review} />
+          <Route path="/submit" component={Submit} />
+        </Router>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,24 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { HashRouter as Router } from 'react-router-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+const storeInstance = createStore(
+  combineReducers({
+    //coming soon :)
+  }),
+  applyMiddleware(logger)
+);
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
